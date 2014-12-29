dd7d3e16bcc047de3e44325eef0db57efaec37d1
Nick Hume <nhume@monetate.com>
2014-12-29 14:25:04 -0500

    ext - make it append a script rather than eval the code

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,29 @@
-$.ajax({
-  url: 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
-  }
-})
+(function() {
+    var request = new XMLHttpRequest();
+    var localScriptUrl = 'https://localhost:3131/' + location.hostname.replace(/^www\./, '') + '.js';
+    request.open('GET', localScriptUrl, true);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            var firstScript = document.getElementsByTagName('script')[0];
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = localScriptUrl;
+
+            firstScript.parentNode.insertBefore(script, firstScript);
+            console.log('Appended the script from dotjs');
+        } else {
+            // We reached our target server, but it returned an error
+            console.warn('no dotjs server found at localhost:3131');
+        }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+      console.error('Could not connect to local dotjs server');
+    };
+
+    request.send();
+
+})();
