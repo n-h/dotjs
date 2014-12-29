5a2168b80830420ceaa95341ccc33cbfecb44b4d
Nick Hume <nhume@monetate.com>
2014-12-29 17:20:36 -0500

    only append script if script is not empty

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -4,15 +4,23 @@
     request.open('GET', localScriptUrl, true);
 
     request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
+        if (request.status == 200) {
             // Success!
             var firstScript = document.getElementsByTagName('script')[0];
             var script = document.createElement('script');
             script.type = 'text/javascript';
             script.src = localScriptUrl;
 
-            firstScript.parentNode.insertBefore(script, firstScript);
-            console.log('Appended the script from dotjs');
+            if (firstScript) {
+                firstScript.parentNode.insertBefore(script, firstScript);
+                console.log('Appended before first script from dotjs');
+            } else if (document.head) {
+                document.head.appendChild(script);
+                console.log('Appended to head script from dotjs');
+            } else if (document.body) {
+                document.body.appendChild(script);
+                console.log('Appended to body script from dotjs');
+            }
         } else {
             // We reached our target server, but it returned an error
             console.warn('no dotjs server found at localhost:3131');
